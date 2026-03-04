158656e1e8c0dab63dc35f207090911793b268f9
cih0802 <codexof13@gmail.com>
2026-03-04 16:35:11 +0900

    dbt에서 증분 데이터만 가져오게함incremental

--- dbt/public_data_mart/models/marts/fct_daily_exchange_rate.sql
+++ dbt/public_data_mart/models/marts/fct_daily_exchange_rate.sql
@@ -1,8 +1,16 @@
--- L2 계층은 조회 성능을 위해 실제 Table 형태로 물리적으로 생성합니다.
-{{ config(materialized='table') }}
+-- L2 계층은 조회 성능을 위해 실제 Table 형태로 물리적으로 생성하며, Incremental 전략을 사용합니다.
+{{ config(
+    materialized='incremental',
+    unique_key=['base_date', 'currency_code']
+) }}
 
 with staging as (
     select * from {{ ref('stg_exchange_rate') }}
+    
+    {% if is_incremental() %}
+    -- 증분 실행 시, 현재 테이블에 적재된 가장 최신 날짜(base_date) 이후의 데이터만 필터링하여 가져옵니다.
+    where search_date >= (select coalesce(max(base_date), '19000101') from {{ this }})
+    {% endif %}
 ),
 
 cleaned_data as (
@@ -31,5 +39,5 @@ select
     ttb,
     tts
 from cleaned_data
--- 하루에 API를 여러 번 호출해 중복 데이터가 생겼을 경우, 최신 적재 데이터 1건만 남김
-qualify row_number() over(partition by base_date, currency_code order by base_date desc) = 1
+-- 하루에 API를 여러 번 호출해 중복 데이터가 생겼을 경우, 최신 적재 시간(loaded_at) 기준 1건만 남김
+qualify row_number() over(partition by base_date, currency_code order by loaded_at desc) = 1
